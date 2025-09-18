c6801211ead9c6a5271b47b7ea33c12994e63f56
Rafiz647 <12345rafiz@gmail.com>
2025-09-18 23:28:17 +0600

    3rd

---
+++ evenandodd.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class evenandodd {
+    public static void main(String[] arg){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a%2==0) System.out.println("Even");
+        else System.out.println("Odd");
+    }
+}
+
+    
+    
+
