502472219f89dc85b82a0a593f6adf4855baa230
Rafiz647 <12345rafiz@gmail.com>
2025-09-18 23:16:17 +0600

    2nd

---
+++ takeInput.java
@@ -0,0 +1,13 @@
+
+
+import java.util.Scanner;
+
+
+
+public class takeInput {
+   public static void main(String[]a){
+    Scanner s=new Scanner(System.in);
+    String n=s.nextLine();
+    System.out.println("Hello, "+n+"!");
+  }
+}
