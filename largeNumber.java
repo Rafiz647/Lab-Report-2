d0904302daf2fc8e66b21e27dd0091c6ed24ff58
Rafiz647 <12345rafiz@gmail.com>
2025-09-18 23:35:20 +0600

    4th

---
+++ largeNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class largeNumber {
+  
+         public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      int var2 = var1.nextInt();
+      int var3 = var1.nextInt();
+      int var4 = var1.nextInt();
+      if (var2 > var3) {
+         System.out.println("A large");
+      } else if (var3 > var4) {
+         System.out.println("B large");
+      } else {
+         System.out.println("C large");
+      }
+
+   }
+    }
+    
+
